b1561a383c0989691723db0d1b4267139f7d30c1
SamuelK0nrad <samuel.ko702@gmail.com>
2025-11-18 21:31:19 +0100

    yet some more problems

--- aoi/munze/grader.cpp
+++ aoi/munze/grader.cpp
@@ -1,8 +1,8 @@
 // #include "munzen.h"
 #include <algorithm>
 #include <bits/stdc++.h>
-#include <exception>
-#include <iostream>
+#include <bitset>
+#include <vector>
 
 using namespace std;
 
@@ -46,7 +46,7 @@ struct domino {
   }
 };
 
-vector<domino> dmap(vector<int> w) {
+vector<domino> d_map(vector<int> &w) {
   vector<domino> res;
 
   for (int i = 0; i < w.size(); ++i) {
@@ -57,113 +57,215 @@ vector<domino> dmap(vector<int> w) {
   return res;
 }
 
-vector<pair<int, int>> sort(vector<int> ew, vector<int> ow) {
-  vector<domino> d = dmap(ew);
+bool l_check_right(domino *start, domino *end) {
+  cout << " start right d: " << start->right_d << "\n";
+  if (start->right_d == nullptr) {
+    return start == end;
+  }
+  return l_check_right(start->right_d, end);
+}
+
+bool l_check_left(domino *start, domino *end) {
+  cout << " start left d: " << start->left_d << "\n";
+  if (start->left_d == nullptr) {
+    cout << " > start: " << start << "\n";
+    cout << " > end: " << end << "\n";
+    return start == end;
+  }
+  return l_check_left(start->left_d, end);
+}
+
+void find(vector<domino> &d, int w, bool u, int count) {
+  domino *left_d = nullptr;
+  domino *right_d = nullptr;
+  bool left = false;
+  bool right = false;
+
+  domino *e_right = nullptr;
+  domino *e_left = nullptr;
+
+  if (w == 4) {
+    left = true;
+    right = true;
+  }
+
+  cout << "\n WERT: " << w << "\n";
+
+  for (int i = 0; i < d.size(); ++i) {
+    if (d[i].w_u) {
+      cout << "de wert: " << d[i].w << "\n";
+      //        cout << "left d: " << left_d << " de left: " << de.left << "
+      //        de left d "
+      //             << de.left_d << " c left: " << left << " \n";
+      if (left_d == nullptr && (d[i].right == left || !u) &&
+          d[i].right_d == nullptr) {
+
+        cout << "check for loop:\n";
+        cout << " left d: " << right_d << "\n";
+        if (right_d != nullptr && count < d.size() &&
+            l_check_left(&d[i], right_d)) {
+          cout << "loop detected!! \n";
+          continue;
+        }
+
+        cout << "found left d \n";
+        left_d = &d[i];
+        if (!u) {
+          u = !u;
+          left = d[i].right;
+          right = !left;
+        }
+        if (e_left != nullptr && e_right == nullptr) {
+          e_right = e_left;
+        }
+      } else if (right_d == nullptr && (d[i].left == right || !u) &&
+                 d[i].left_d == nullptr) {
+
+        cout << "check for loop:\n";
+        cout << " left d: " << left_d << "\n";
+        if (left_d != nullptr && count < d.size() &&
+            l_check_right(&d[i], left_d)) {
+          cout << "loop detected!! \n";
+          continue;
+        }
+
+        cout << "found right d\n";
+        right_d = &d[i];
+        if (!u) {
+          u = !u;
+          right = d[i].left;
+          left = !right;
+        }
+      }
+    } else {
+      cout << "de has no final positioning: " << d[i].w << "\n";
+      if (left_d == nullptr && e_left == nullptr) {
+        e_left = &d[i];
+        cout << "set e left \n";
+      } else if (right_d == nullptr && e_right == nullptr) {
+        e_right = &d[i];
+        cout << "set e right: " << e_right << " w: " << e_right->w << "\n";
+      }
+    }
+  }
+
+  if (!u) {
+    left = true;
+    right = false;
+    u = true;
+  }
+
+  if (left_d == nullptr) {
+    if (e_left == nullptr) {
+      throw "something went wrong in sort no possible next domino left";
+    }
+    cout << "left d is null\n";
+    left_d = e_left;
+
+    e_left->left = !left;
+    e_left->right = left;
+    e_left->w_u = true;
+  }
+  if (right_d == nullptr) {
+    if (e_right == nullptr) {
+      throw "something went wrong in sort no possible next domino right";
+    }
+    cout << "right d is null e right: " << e_right << " w " << e_right->w
+         << "\n";
+    right_d = e_right;
+
+    e_right->right = !right;
+    e_right->left = right;
+    e_right->w_u = true;
+  }
+
+  left_d->right_d = right_d;
+  right_d->left_d = left_d;
+
+  cout << " |||: " << left_d->left << " " << left_d->right << " "
+       << left_d->right_d->left << " " << left_d->right_d->right << " \n";
+}
+
+vector<int> sort(vector<int> &ew, vector<int> &ow) {
+  vector<domino> d = d_map(ew);
 
   int dk = 0;
+
+  int count = 0;
 
   for (int e : ow) {
     if (e == 3) {
       dk++;
       continue;
     }
-
-    domino *left_d = nullptr;
-    domino *right_d = nullptr;
-    bool left = false;
-    bool right = false;
-
-    domino *e_right = nullptr;
-    domino *e_left = nullptr;
-
-    if (e == 4) {
-      left = true;
-      right = true;
-    }
-
-    cout << "\n WERT: " << e << "\n";
-
-    for (int i = 0; i < d.size(); ++i) {
-      cout << "e right: " << e_right << "\n";
-      if (d[i].w_u) {
-        cout << "de wert: " << d[i].w << "\n";
-        //        cout << "left d: " << left_d << " de left: " << de.left << "
-        //        de left d "
-        //             << de.left_d << " c left: " << left << " \n";
-        if (left_d == nullptr && d[i].right == left &&
-            d[i].right_d == nullptr) {
-          cout << "found left d \n";
-          left_d = &d[i];
-          if (e_left != nullptr && e_right == nullptr) {
-            e_right = e_left;
-          }
-        } else if (right_d == nullptr && d[i].left == right &&
-                   d[i].left_d == nullptr) {
-          cout << "found right d\n";
-          right_d = &d[i];
-        }
-      } else {
-        cout << "de has no final positioning: " << d[i].w << "\n";
-        if (left_d == nullptr && e_left == nullptr) {
-          e_left = &d[i];
-          cout << "set e left \n";
-        } else if (right_d == nullptr && e_right == nullptr) {
-          e_right = &d[i];
-          cout << "set e right: " << e_right << " w: " << e_right->w << "\n";
-        }
-      }
-    }
-
-    if (left_d == nullptr) {
-      if (e_left == nullptr) {
-        throw "something went wrong in sort no possible next domino left";
-      }
-      cout << "left d is null\n";
-      left_d = e_left;
-
-      e_left->left = !left;
-      e_left->right = left;
-      e_left->w_u = true;
-    }
-    if (right_d == nullptr) {
-      if (e_right == nullptr) {
-        throw "something went wrong in sort no possible next domino right";
-      }
-      cout << "right d is null e right: " << e_right << " w " << e_right->w
-           << "\n";
-      right_d = e_right;
-
-      e_right->right = !right;
-      e_right->left = right;
-      e_right->w_u = true;
-    }
-
-    left_d->right_d = right_d;
-    right_d->left_d = left_d;
-
-    cout << " |||: " << left_d->left << " " << left_d->right << " "
-         << left_d->right_d->left << " " << left_d->right_d->right << " \n";
+    count++;
+    find(d, e, true, count);
   }
-
-  int i = 0;
+  for (int i = 0; i < dk; ++i) {
+    count++;
+    find(d, 3, false, count);
+  }
 
   domino *next = &d[0];
 
-  while (i <= ew.size() && next != nullptr) {
+  cout << " ||| >> : " << next->left_d->left << next->left_d->right
+       << next->left << next->right << next->right_d->left
+       << next->right_d->right << next->right_d->right_d->left
+       << next->right_d->right_d->right << " \n";
+
+  cout << "\n";
+
+  vector<int> res;
+
+  int i = 0;
+  do {
     cout << i << ": \n"
          << " left: " << next->left << "\n right: " << next->right
          << "\n wert: " << next->w << "\n";
 
-    i++;
-    next = next->left_d;
-  }
+    cout << " >right: " << next->right_d << "\n >left: " << next->left_d
+         << " \n";
 
-  return {};
+    cout << next->left << next->right << "\n";
+
+    if (next->left) {
+      res.push_back(2);
+    } else {
+      res.push_back(1);
+    }
+
+    if (next->right) {
+      res.push_back(2);
+    } else {
+      res.push_back(1);
+    }
+
+    i += 2;
+    next = next->right_d;
+  } while (next != &d[0]);
+
+  cout << "\n";
+
+  return res;
+}
+
+int find_start(bitset<10001> b) {
+  vector<int> w = wiege({{0}});
+  bitset<10001> ps;
+  ps = b;
+  if (w[0] == 1) {
+    ps.flip();
+  }
+  return 0;
 }
 
 std::vector<int> bestimme_werte(int n) {
-  if (n % 2 == 1)
-    return {};
+  bool odd = false;
+  int last = 0;
+  if (n % 2 == 1) {
+    odd = true;
+    last = wiege({{n}})[0];
+  }
 
   // implementiere hier deine Lösung.
   // Mit wiege(std::vector<std::vector<int>> paket) kannst du Abfragen machen.
@@ -183,9 +285,12 @@ std::vector<int> bestimme_werte(int n) {
   vector<int> e_pairs_res = wiege(e_pairs);
   vector<int> o_pairs_res = wiege(o_pairs);
 
-  sort(e_pairs_res, o_pairs_res);
+  auto c = sort(e_pairs_res, o_pairs_res);
+  if (odd) {
+    c.push_back(last);
+  }
 
-  return e_pairs_res;
+  return c;
 }
 
 int main() {
